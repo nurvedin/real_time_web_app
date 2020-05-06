65ee36f1f7a02a7e193bac47d52c589940668f52
Nurvedin Dizdarevic <nurvedin@hotmail.com>
2020-05-06 12:40:36 +0200

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -8,7 +8,7 @@ const fetch = require('node-fetch')
 // personal token d65a11ab33e2a2ef0fa68df8aad8002713ea162d
 
 const getIssues = async (req, res) => {
-  const repos = await fetch('https://api.github.com/repos/1dv523/nd222dp-examination-3/issues?access_token=760fab9982c249e11253d603bf3dc6d46eb75a75')
+  const repos = await fetch('github repo with access token')
   const viewData = await repos.json()
   res.render('home/index', { viewData: viewData })
 }
